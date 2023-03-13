cd0252b8c352c2f0cbf01f9f593d96cdbfe816ab
ToveLissner <tove_lissner@hotmail.com>
2023-03-13 14:33:38 +0100

    Booking

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { getRestaurant, handleClick } from './services/restaurantService';
+import { handleClick } from './services/restaurantService';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
-
-// createRestaurant();
+import { Booking } from './components/Booking';
 
 function App() {
   return (
     <div>
+      <Header/>
       <main>
         <Outlet></Outlet>
       </main>
       <button onClick={handleClick}>Test</button>
+      <Booking/>
     </div>
-  );
+  )
 }
 
 export default App;
