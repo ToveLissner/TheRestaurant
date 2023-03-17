fdd0bec78582f7d4c61bb340556909cd5d38966e
ToveLissner <tove_lissner@hotmail.com>
2023-03-17 13:13:57 +0100

    styling

--- src/components/styled/Div.tsx
+++ src/components/styled/Div.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const AdminButtonDiv = styled.div`
   display: flex;
-  justify_content: space-around;
+  justify-content: flex-end;
   width: 40vh;
 `;
