b0bff8cd316783ea4c5b1ac5f8ea0852af9ad1c8
yuen0 <nicolaslopezpp@gmail.com>
2023-03-17 16:47:36 +0100

    implemented fix for double rendering of header and footer

--- src/components/styled/Header/Menu/Menu.tsx
+++ src/components/styled/Header/Menu/Menu.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { StyledMenuLinks, StyledMenu } from "./Menu.styled";
 
 interface Props {
-  open: boolean;
+	open: boolean;
 }
 
 const Menu: React.FC<Props> = ({ open }) => {
-  return (
-    <StyledMenu open={open}>
-      <StyledMenuLinks to="/">Start</StyledMenuLinks>
-      <StyledMenuLinks to="../../booking">Boka</StyledMenuLinks>
-      <StyledMenuLinks to="#">Meny</StyledMenuLinks>
-      <StyledMenuLinks to="../../contact">Kontakt</StyledMenuLinks>
-      <StyledMenuLinks to="../../admin">Admin</StyledMenuLinks>
-    </StyledMenu>
-  );
+	return (
+		<StyledMenu open={open}>
+			<StyledMenuLinks to="/">Start</StyledMenuLinks>
+			<StyledMenuLinks to="../../booking">Boka</StyledMenuLinks>
+			<StyledMenuLinks to="#">Meny</StyledMenuLinks>
+			<StyledMenuLinks to="../../contact">Kontakt</StyledMenuLinks>
+			<StyledMenuLinks to="../../admin">Admin</StyledMenuLinks>
+		</StyledMenu>
+	);
 };
 
 export default Menu;
