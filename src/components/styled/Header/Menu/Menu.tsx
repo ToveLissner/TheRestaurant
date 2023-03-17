65ad3f7818298d75dabe4b9c55c5ff9982efe374
Clarence Renström <clarre.renstrom@gmail.com>
2023-03-17 10:02:17 +0100

    added sort and search function to admin

--- src/components/styled/Header/Menu/Menu.tsx
+++ src/components/styled/Header/Menu/Menu.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { StyledMenuLinks, StyledMenu } from "./Menu.styled";
 
 interface Props {
-	open: boolean;
+  open: boolean;
 }
 
 const Menu: React.FC<Props> = ({ open }) => {
-	return (
-		<StyledMenu open={open}>
-			<StyledMenuLinks to="../../">Start</StyledMenuLinks>
-			<StyledMenuLinks to="#">Boka</StyledMenuLinks>
-			<StyledMenuLinks to="#">Meny</StyledMenuLinks>
-			<StyledMenuLinks to="../../pages/Contact">Kontakt</StyledMenuLinks>
-			<StyledMenuLinks to="#">Admin</StyledMenuLinks>
-		</StyledMenu>
-	);
+  return (
+    <StyledMenu open={open}>
+      <StyledMenuLinks to="../../">Start</StyledMenuLinks>
+      <StyledMenuLinks to="#">Boka</StyledMenuLinks>
+      <StyledMenuLinks to="#">Meny</StyledMenuLinks>
+      <StyledMenuLinks to="../../pages/Contact">Kontakt</StyledMenuLinks>
+      <StyledMenuLinks to="../../pages/Admin">Admin</StyledMenuLinks>
+    </StyledMenu>
+  );
 };
 
 export default Menu;
